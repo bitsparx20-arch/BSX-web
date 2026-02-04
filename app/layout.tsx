479779ad3fa61cd9aa6d369ff1474c4623f470ae
Bitsparx <bitsparx20@gmail.com>
2026-02-04 17:17:23 +0530

    change16

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) - loads on every page */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1HBTYKP2D4"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1HBTYKP2D4');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${plusJakartaSans.variable} font-sans antialiased`}
       >
